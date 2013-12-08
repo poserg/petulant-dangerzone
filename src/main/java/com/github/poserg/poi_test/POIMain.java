0228001f349e3b9e5508846d4b59d2366b1f386b
Sergey Popov <poserg@gmail.com>
2013-12-09 00:17:32 +0400

    Created styles

--- src/main/java/com/github/poserg/poi_test/POIMain.java
+++ src/main/java/com/github/poserg/poi_test/POIMain.java
@@ -5,12 +5,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.FontUnderline;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 /**
  * @author Sergey Popov
@@ -18,6 +25,10 @@ import org.apache.poi.ss.usermodel.Workbook;
  */
 public class POIMain {
 
+	private static final String TABLE_HEADER = "table_header";
+	private static final String TEXT_STYLE = "text_style";
+	private static final String SUBTITLE_STYLE = "subtitle_style";
+	private static final String TITLE_STYLE = "title_style";
 	private static final String DEPARTMENT_REPORT = "Отчет по заявкам в разрезе ведомств";
 
 	/**
@@ -28,25 +39,35 @@ public class POIMain {
 		
 		Sheet sheet = wb.createSheet(DEPARTMENT_REPORT);
 		sheet.setFitToPage(true);
+		sheet.setDisplayGridlines(false);
+		
+		Map<String, CellStyle> styles = createStyles(wb);
 
 		Row titleRow = sheet.createRow(1);
 		Cell cell = titleRow.createCell(0);
 		cell.setCellValue(DEPARTMENT_REPORT);
+		cell.setCellStyle(styles.get(TITLE_STYLE));
+        titleRow.setHeightInPoints(45);
+        sheet.addMergedRegion(CellRangeAddress.valueOf("$A$2:$L$2"));
 		
 		// Date
 		Row dateRow = sheet.createRow(3);
 		Cell dateLabelCell = dateRow.createCell(0);
 		dateLabelCell.setCellValue("Дата формирования отчета:");
+		dateLabelCell.setCellStyle(styles.get(SUBTITLE_STYLE));
 		Cell dateCell = dateRow.createCell(1);
 		Date today = new Date();
 		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 		dateCell.setCellValue(formatter.format(today));
+		dateCell.setCellStyle(styles.get(TEXT_STYLE));
 		
 		// Date interval
 		Row dateIntervalRow = sheet.createRow(5);
 		Cell dateIntervalLabel = dateIntervalRow.createCell(0);
 		dateIntervalLabel.setCellValue("Период:");
 		Cell dateInterval = dateIntervalRow.createCell(1);
+		dateInterval.setCellStyle(styles.get(TABLE_HEADER));
+		sheet.setColumnWidth(1, 50*256);
 		
 		SimpleDateFormat formatter2 = new SimpleDateFormat("dd.MM.yyyy");
 		String date1 = formatter2.format(today);
@@ -90,4 +111,92 @@ public class POIMain {
 		tableHeader.createCell(1).setCellValue("Всего");
 	}
 
+	private static Map<String, CellStyle> createStyles(Workbook wb) {
+		Map<String, CellStyle> styles = new HashMap<String, CellStyle>();
+		
+		CellStyle style;
+		
+		Font titleFont = wb.createFont();
+		titleFont.setUnderline(FontUnderline.SINGLE.getByteValue());
+		titleFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		titleFont.setFontHeightInPoints((short) 16);
+		style = wb.createCellStyle();
+		style.setFont(titleFont);
+		styles.put(TITLE_STYLE, style);
+		
+		Font subtitleFont = wb.createFont();
+		subtitleFont.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		subtitleFont.setFontHeightInPoints((short) 11);
+		CellStyle subtitleStyle = wb.createCellStyle();
+		subtitleStyle.setFont(subtitleFont);
+		styles.put(SUBTITLE_STYLE, subtitleStyle);
+		
+		Font textFont = wb.createFont();
+		textFont.setFontHeightInPoints((short) 11);
+		CellStyle textStyle = wb.createCellStyle();
+		textStyle.setFont(textFont);
+		styles.put(TEXT_STYLE, textStyle);
+		
+		
+		CellStyle tableHeaderStyle = wb.createCellStyle();
+		tableHeaderStyle.setFont(subtitleFont);
+		tableHeaderStyle.setAlignment(CellStyle.ALIGN_CENTER);
+		setBorders(tableHeaderStyle);
+		setBackgroud(tableHeaderStyle, IndexedColors.GREY_40_PERCENT);
+		styles.put(TABLE_HEADER, tableHeaderStyle);
+		
+		CellStyle subtableTitle = wb.createCellStyle();
+		subtableTitle.setFont(subtitleFont);
+		setBackgroud(subtableTitle, IndexedColors.GREY_25_PERCENT);
+		setBorders(subtableTitle);
+		
+		CellStyle tableContentOddStyle = wb.createCellStyle();
+		tableContentOddStyle.setFont(textFont);
+		setBorders(tableContentOddStyle);
+		styles.put("tabel_content_odd", tableContentOddStyle);
+		
+		CellStyle tableContentEvenStyle = wb.createCellStyle();
+		tableContentEvenStyle.setFont(textFont);
+		setBackgroud(tableContentEvenStyle, IndexedColors.GREY_25_PERCENT);
+		setBorders(tableContentEvenStyle);
+		styles.put("table_content_even", tableContentEvenStyle);
+		
+		CellStyle totalTitleCellStyle = wb.createCellStyle();
+		totalTitleCellStyle.setFont(subtitleFont);
+		totalTitleCellStyle.setAlignment(CellStyle.ALIGN_RIGHT);
+		setBorders(totalTitleCellStyle, CellStyle.BORDER_DOUBLE);
+		setBackgroud(totalTitleCellStyle, IndexedColors.GREY_40_PERCENT);
+		styles.put("total_title", totalTitleCellStyle);
+		
+		CellStyle totalCellStyle = wb.createCellStyle();
+		totalCellStyle.setFont(subtitleFont);
+		totalCellStyle.setAlignment(CellStyle.ALIGN_CENTER);
+		setBorders(totalCellStyle, CellStyle.BORDER_DOUBLE);
+		setBackgroud(totalTitleCellStyle, IndexedColors.GREY_40_PERCENT);
+		styles.put("total", totalCellStyle);
+		
+		return styles;
+	}
+
+	/**
+	 * @param tableHeaderStyle
+	 */
+	private static void setBackgroud(CellStyle tableHeaderStyle, IndexedColors color) {
+		tableHeaderStyle.setFillForegroundColor(color.getIndex());
+		tableHeaderStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+	}
+
+	/**
+	 * @param tableHeaderStyle
+	 */
+	private static void setBorders(CellStyle tableHeaderStyle) {
+		setBorders(tableHeaderStyle, CellStyle.BORDER_MEDIUM);
+	}
+	
+	private static void setBorders(CellStyle tableHeaderStyle, short style) {
+		tableHeaderStyle.setBorderTop(CellStyle.BORDER_MEDIUM);
+		tableHeaderStyle.setBorderLeft(CellStyle.BORDER_MEDIUM);
+		tableHeaderStyle.setBorderRight(CellStyle.BORDER_MEDIUM);
+		tableHeaderStyle.setBorderBottom(CellStyle.BORDER_MEDIUM);
+	}
 }
